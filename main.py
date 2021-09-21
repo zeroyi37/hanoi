f07c4650f03cdc91d09b55cdc94dbbe3e7a0e3de
zeroyi37 <73918045+zeroyi37@users.noreply.github.com>
2021-09-21 12:57:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def hannt(n, x, y, z):
     if n == 1:
         print(x, '-->', z)
     else:
-        hannt(n-1, x, z, y)#调用函数将X塔上1~n-1个圆盘移到Y塔上，以Z塔为辅助塔
+        hannt(n-1, x, z, y)#将X塔上1~n-1个圆盘移到Y塔上，以Z塔为辅助塔
         print(x, '-->', z)#将第n个圆盘移到Z塔上
         hannt(n-1, y, x, z)#把Y塔上编号1~n-1的圆盘移到Z上，以X为辅助塔
 
