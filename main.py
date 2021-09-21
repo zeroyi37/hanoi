ea9af0da836bc0bce57bb3500122d0359a0f68ac
zeroyi37 <73918045+zeroyi37@users.noreply.github.com>
2021-09-21 12:58:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def hannt(n, x, y, z):
+def hanoi(n, x, y, z):
     if n == 1:
         print(x, '-->', z)
     else:
-        hannt(n-1, x, z, y)#将X塔上1~n-1个圆盘移到Y塔上，以Z塔为辅助塔
+        hanoi(n-1, x, z, y)#将X塔上1~n-1个圆盘移到Y塔上，以Z塔为辅助塔
         print(x, '-->', z)#将第n个圆盘移到Z塔上
-        hannt(n-1, y, x, z)#把Y塔上编号1~n-1的圆盘移到Z上，以X为辅助塔
+        hanoi(n-1, y, x, z)#把Y塔上编号1~n-1的圆盘移到Z上，以X为辅助塔
 
 n = int(input('请输入汉诺塔层数：'))
-hannt(n, 'X', 'Y', 'Z')
+hanoi(n, 'X', 'Y', 'Z')
